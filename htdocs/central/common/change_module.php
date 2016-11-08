9a6b205c0732c4014dfd3f3323e0218997277be3
lindicell <lindice@hotmail.com>
2016-11-08 15:54:45 -0200

    adicionado chaves no else

--- htdocs/central/common/change_module.php
+++ htdocs/central/common/change_module.php
@@ -4,9 +4,9 @@ include("get_post.php");
 $_SESSION["super_user"]=$_SESSION["permiso"];
 
 // to see if a separate window is activate
-if (isset($_SESSION["newindow"]))
-	$lit="&newindow=s";
-else
-	$lit="";
+if (isset($_SESSION["newindow"])){
+	$lit="&newindow=s";}
+else{
+	$lit="";}
 header ("Location: ../common/inicio.php?reinicio=s$lit&modulo=".$arrHttp["modulo"]);
 ?>
