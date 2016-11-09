6fb199cba1994d40518c1c849ae2cbcc1a563a03
lindicell <lindice@hotmail.com>
2016-11-09 15:44:36 -0200

    .

--- htdocs/central/config.php
+++ htdocs/central/config.php
@@ -22,7 +22,7 @@ $max_cn_length=1;
 
 //Colocar Y en esta variable si se quiere llevar un log de todas las transacciones realizadas sobre la base de datos.
 //Para que funcione en la carpeta de la base de datos debe existir una subcarpeta llamada log
-$log="Y";
+$log="N";
 
 
 //$db_path="/bases_abcd/bases/";
