49c3bb72acfc651c610de752038be93794db34e8
lindicell <lindice@hotmail.com>
2016-11-09 15:49:08 -0200

    Corrigido linha 226

--- htdocs/central/common/inicio.php
+++ htdocs/central/common/inicio.php
@@ -10,9 +10,13 @@ require_once ("../config.php");
 //foreach ($arrHttp as $var=>$value) echo "$var=$value<br>";die;
 $valortag = Array();
 
-function CambiarPassword($Mfn,$new_password){global $xWxis,$Wxis,$db_path,$wxisUrl,$MD5;
-	if (isset($MD5) and $MD5==1 ){		$new_password=md5($new_password);	}
-	$ValorCapturado="d30<30 0>".$new_password."</30>";	$ValorCapturado=urlencode($ValorCapturado);
+function CambiarPassword($Mfn,$new_password){
+global $xWxis,$Wxis,$db_path,$wxisUrl,$MD5;
+	if (isset($MD5) and $MD5==1 ){
+		$new_password=md5($new_password);
+	}
+	$ValorCapturado="d30<30 0>".$new_password."</30>";
+	$ValorCapturado=urlencode($ValorCapturado);
 	$IsisScript=$xWxis."actualizar.xis";
   	$query = "&base=acces&cipar=$db_path"."par/acces.par&login=".$_SESSION["login"]."&Mfn=" . $Mfn."&Opcion=actualizar&ValorCapturado=".$ValorCapturado;
     echo $query;
@@ -28,15 +32,19 @@ global $llamada, $valortag,$maxmfn,$arrHttp,$OS,$Bases,$xWxis,$Wxis,$Mfn,$db_pat
 	$tag= "";
 	$IsisScript=$xWxis."login.xis";
 	$pass=$arrHttp["password"];
-	if (isset($MD5) and $MD5==1){		$pass=md5($pass);	}
+	if (isset($MD5) and $MD5==1){
+		$pass=md5($pass);
+	}
 	$query = "&base=acces&cipar=$db_path"."par/acces.par"."&login=".$arrHttp["login"]."&password=".$pass;
 	include("wxis_llamar.php");
 
 	 foreach ($contenido as $linea){
 
-	 	if ($ic==-1){	    	$ic=1;
+	 	if ($ic==-1){
+	    	$ic=1;
 	    	$pos=strpos($linea, '##LLAVE=');
-	    	if (is_integer($pos)) {	     		$llave_pft=substr($linea,$pos+8);
+	    	if (is_integer($pos)) {
+	     		$llave_pft=substr($linea,$pos+8);
 	     		$pos=strpos($llave_pft, '##');
 	     		$llave_pft=substr($llave_pft,0,$pos);
 			}
@@ -76,31 +84,42 @@ global $arrHttp,$valortag,$Path,$xWxis,$session_id,$Permiso,$msgstr,$db_path,$no
   		else{
   			$fecha="";
   		$today=date("Ymd");
-  		if (trim($fecha)!=""){  			if ($today>$fecha){
+  		if (trim($fecha)!=""){
+  			if ($today>$fecha){
   				header("Location: ../../index.php?login=N");
-  				die;  			}  		}
+  				die;
+  			}
+  		}
   	}
   		$value=substr($value,2);
   		$ix=strpos($value,'^');
   		$Perfil=substr($value,0,$ix);
-    	if (!file_exists($db_path."par/profiles/".$Perfil)){    		echo "missing ". $db_path."par/profiles/".$Perfil;
-    		die;    	}
+    	if (!file_exists($db_path."par/profiles/".$Perfil)){
+    		echo "missing ". $db_path."par/profiles/".$Perfil;
+    		die;
+    	}
     	$profile=file($db_path."par/profiles/".$Perfil);
     	unset($_SESSION["profile"]);
     	unset($_SESSION["permiso"]);
     	unset($_SESSION["login"]);
     	$_SESSION["profile"]=$Perfil;
     	$_SESSION["login"]=$arrHttp["login"];
-    	foreach ($profile as $value){    		$value=trim($value);
-    		if ($value!=""){    			$key=explode("=",$value);
-    			$_SESSION["permiso"][$key[0]]=$key[1];    		}
+    	foreach ($profile as $value){
+    		$value=trim($value);
+    		if ($value!=""){
+    			$key=explode("=",$value);
+    			$_SESSION["permiso"][$key[0]]=$key[1];
+    		}
     	}
         if (isset($valortag[70])){
         	$library=$valortag[70];
         	$_SESSION["library"]=$library;
-        }else{        	unset ($_SESSION["library"]);        }
+        }else{
+        	unset ($_SESSION["library"]);
+        }
  	}else{
- 		if ($arrHttp["login"]==$adm_login and $arrHttp["password"]==$adm_password){ 			$Perfil="adm";
+ 		if ($arrHttp["login"]==$adm_login and $arrHttp["password"]==$adm_password){
+ 			$Perfil="adm";
  			unset($_SESSION["profile"]);
     		unset($_SESSION["permiso"]);
     		unset($_SESSION["login"]);
@@ -115,7 +134,8 @@ global $arrHttp,$valortag,$Path,$xWxis,$session_id,$Permiso,$msgstr,$db_path,$no
     				$_SESSION["permiso"][$key[0]]=$key[1];
     			}
     		}
-    	}else{ 			echo "<script>\n";
+    	}else{
+ 			echo "<script>\n";
  			if (isset($_SESSION["HOME"]))
  				echo "self.location.href=\"".$_SESSION["HOME"]."?login=N\"\n";
  			else{
@@ -193,12 +213,241 @@ include("../lang/acquisitions.php");
     	die;
     }
 	$Permiso=$_SESSION["permiso"];
-	if (isset($arrHttp["Opcion"]) and $arrHttp["Opcion"]=="chgpsw"){		CambiarPassword($arrHttp["Mfn"],$arrHttp["new_password"]);
+	if (isset($arrHttp["Opcion"]) and $arrHttp["Opcion"]=="chgpsw"){
+		CambiarPassword($arrHttp["Mfn"],$arrHttp["new_password"]);
 		if (isset($_SESSION["HOME"]))
 			$retorno=$_SESSION["HOME"];
 		else
 			$retorno="../../index.php";
-		header("Location: $retorno?login=P");	}else{
+		header("Location: $retorno?login=P");
+	}else{
+?>
+
+    <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-gitlab"></i> <span>Suíte Saber 17</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile">
+              <div class="profile_pic">
+                <img src="images/user.png" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Olá,</span>
+                <h2>João</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+       <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>Administração</h3>
+
+
+
+<?php
 		include("homepage.php");
+		include("menucirculation.php");
+		include("menuacquisitions.php");
+
 	}
 
+?>
+
+
+    
+             </div>
+            </div>
+            <!-- /sidebar menu -->
+    
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+             <label>Base de dados ativa</label>
+                  <select>
+                    <option>Base Bibliografica</option>
+                    <option>Base Arquivo</option>
+                    <option>Base Autoridades</option>
+                  </select>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                
+              </div>
+                 
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/user.png" alt="">João
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Profile</a></li>
+                    <li>
+
+                    </li>
+                    <li><a href="javascript:;">Help</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        
+  <!-- page content -->
+        <div class="right_col" role="main" id="content">
+          <div class="">
+			
+			<iframe name="content" frameborder="no" scrolling="auto" width="100%" height="100%" src="/site"></iframe>
+
+          </div>
+        </div>
+        <!-- /page content -->
+        
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Suite saber 
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+   
+    <!-- jQuery -->
+    <script src="../css/<?php echo $css_name?>/js/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../css/<?php echo $css_name?>/js/bootstrap.min.js"></script>
+     <!-- bootstrap-daterangepicker -->
+    <script src="../css/<?php echo $css_name?>/js/moment/moment.min.js"></script>
+    <script src="../css/<?php echo $css_name?>/js/datepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../css/<?php echo $css_name?>/js/custom.min.js"></script>
+
+  
+    <!-- bootstrap-daterangepicker -->
+    <script>
+      $(document).ready(function() {
+
+        var cb = function(start, end, label) {
+          console.log(start.toISOString(), end.toISOString(), label);
+          $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        };
+
+        var optionSet1 = {
+          startDate: moment().subtract(29, 'days'),
+          endDate: moment(),
+          minDate: '01/01/2012',
+          maxDate: '12/31/2015',
+          dateLimit: {
+            days: 60
+          },
+          showDropdowns: true,
+          showWeekNumbers: true,
+          timePicker: false,
+          timePickerIncrement: 1,
+          timePicker12Hour: true,
+          ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+          },
+          opens: 'left',
+          buttonClasses: ['btn btn-default'],
+          applyClass: 'btn-small btn-primary',
+          cancelClass: 'btn-small',
+          format: 'MM/DD/YYYY',
+          separator: ' to ',
+          locale: {
+            applyLabel: 'Submit',
+            cancelLabel: 'Clear',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+          }
+        };
+        $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+        $('#reportrange').daterangepicker(optionSet1, cb);
+        $('#reportrange').on('show.daterangepicker', function() {
+          console.log("show event fired");
+        });
+        $('#reportrange').on('hide.daterangepicker', function() {
+          console.log("hide event fired");
+        });
+        $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
+          console.log("apply event fired, start/end dates are " + picker.startDate.format('MMMM D, YYYY') + " to " + picker.endDate.format('MMMM D, YYYY'));
+        });
+        $('#reportrange').on('cancel.daterangepicker', function(ev, picker) {
+          console.log("cancel event fired");
+        });
+        $('#options1').click(function() {
+          $('#reportrange').data('daterangepicker').setOptions(optionSet1, cb);
+        });
+        $('#options2').click(function() {
+          $('#reportrange').data('daterangepicker').setOptions(optionSet2, cb);
+        });
+        $('#destroy').click(function() {
+          $('#reportrange').data('daterangepicker').remove();
+        });
+      });
+    </script>
+    <!-- /bootstrap-daterangepicker -->
+
+    <!-- gauge.js -->
+    <script>
+      var opts = {
+          lines: 12,
+          angle: 0,
+          lineWidth: 0.4,
+          pointer: {
+              length: 0.75,
+              strokeWidth: 0.042,
+              color: '#1D212A'
+          },
+          limitMax: 'false',
+          colorStart: '#1ABC9C',
+          colorStop: '#1ABC9C',
+          strokeColor: '#F0F3F3',
+          generateGradient: true
+      };
+      var target = document.getElementById('foo'),
+          gauge = new Gauge(target).setOptions(opts);
+
+      gauge.maxValue = 6000;
+      gauge.animationSpeed = 32;
+      gauge.set(3200);
+      gauge.setTextField(document.getElementById("gauge-text"));
+    </script>
+    <!-- /gauge.js -->
+  </body>
