4b2cf66c920dd482aa39bbda9f0ee4d87803ec7b
lindicell <lindice@hotmail.com>
2016-11-16 14:58:27 -0200

    .

--- htdocs/central/common/footer.php
+++ htdocs/central/common/footer.php
@@ -6,23 +6,18 @@
         ?>
 
 
-				<strong>ABCD 1.5.1  </strong>
-				<span><?php if (isset($def["LEGEND1"])) echo $def["LEGEND1"]; ?></span>
+				<p>  &copy; <?php echo date("Y"); ?> - ABCD
+				<?php if (isset($def["VERSION"])) echo $def["VERSION"]; ?>
+        <p>
 				<?php if(isset($def["URL1"])){
-					echo "<a href=".$def["URL1"]." target=_blank>". $def["URL1"]."</a>";
+					echo "<a href=".$def["URL1"]." target=_blank>".$def["LEGEND1"]."</a>";
 				}
 				if(isset($def["URL2"])){
-					echo "<a href=".$def["URL2"]." target=_blank>". $def["LEGEND2"]."</a>";
+            echo "| <a href=".$def["URL2"]." target=_blank>". $def["LEGEND2"]."</a>";
 				}
-				?>
-			</div>
-			<?php
-			if (!isset($def["LEGEND2"])) $def["LEGEND2"]=$def["LEGEND1"];
-			if (isset($def["URL2"]) and isset($def["LEGEND2"])){
-				echo "<div class=\"distributorLogo\">";
-				echo "<a href=".$def["URL2"]." target=_blank><span>". $def["LEGEND2"]."</span></a> ";
-			}
+				
 			?>
+      </p>
 
 
 		  <!-- jQuery -->
--- htdocs/login.php
+++ htdocs/login.php
@@ -21,27 +21,40 @@ include ("$app_path/lang/lang.php");
 		$css_name.="/";
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br">
-	<head>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<title><?php echo $institution_name; ?></title>
-		<meta http-equiv="Expires" content="-1" />
-		<meta http-equiv="pragma" content="no-cache" />
-		<META http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-		<meta http-equiv="Content-Language" content="pt-br" />
-		<meta name="robots" content="all" />
-		<meta http-equiv="keywords" content="" />
-		<meta http-equiv="description" content="" />
-		<!-- Stylesheets -->
-		<link rel="stylesheet" rev="stylesheet" href="<?php echo $app_path?>/css/<?php echo $css_name?>/template.css" type="text/css" media="screen"/>
-		<!--[if IE]>
-			<link rel="stylesheet" rev="stylesheet" href="<?php echo $app_path?>/css/bugfixes_ie.css" type="text/css" media="screen"/>
-		<![endif]-->
-		<!--[if IE 6]>
-			<link rel="stylesheet" rev="stylesheet" href="<?php echo $app_path?>/css/bugfixes_ie6.css" type="text/css" media="screen"/>
-		<![endif]-->
+
+  <!-- Bootstrap -->
+    <link href="<?php echo $app_path;?>/css/<?php echo $css_name?>/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+   <script src="https://use.fontawesome.com/4c37ce0a9e.js"></script>
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/skins/flat/green.css" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/jqvmap.min.css" rel="stylesheet"/>
+
+    <!-- Custom Theme Style -->
+    <link href="<?php echo $app_path?>/css/<?php echo $css_name?>/css/custom.css" rel="stylesheet">
+
+
+		<!--Font Awesome-->
+    <script src="https://use.fontawesome.com/4c37ce0a9e.js"></script>
+	
 <script src=<?php echo $app_path?>/dataentry/js/lr_trim.js></script>
 <script language="javascript">
 
@@ -82,31 +95,105 @@ function Enviar(){
 
 </script>
 </head>
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <form name="administra" onsubmit="javascript:return false" method="post" action="<?php echo $app_path?>/common/inicio.php?reinicio=s&base=<?php echo $basedefault; ?>">
+				<input type=hidden name=Opcion value=admin>
+				<input type=hidden name=cipar value=acces.par>
+				<value=<?php echo $arrHttp["lang"]?>>
+              <h1>Entrar</h1>
+
+              <div>
+                <input type="text" name="login" id="user" class="form-control" placeholder="Username" required="" />
+              </div>
+
+              <div>
+                <input type="password" id="pwd"  name="password" class="form-control" placeholder="Password" required="" />
+              </div>
+
+              <div>
+
+
+	
+<?php
+if (file_exists("dbpath.dat")){
+	$fp=file("dbpath.dat");
+	echo $msgstr["database_dir"].": <select name=db_path>\n";
+	foreach ($fp as $value){
+		if (trim($value)!=""){
+			$v=explode('|',$value);
+			$v[0]=trim($v[0]);
+			echo "<Option value=".trim($v[0]).">".$v[1]."\n";
+		}
+
+	}
+	echo "</select>	";
+}
+?>
+
+<div class="form-group">
+			<input type="checkbox" name="newindow" value=
+<?php
+if (isset($open_new_window) and $open_new_window=="Y")
+	echo "Y checked";
+else
+	echo "N";
+?> />
+			<label for="setCookie" class="inline"><?php echo $msgstr["openwindow"]?></label>
+</div>
+                <a href="javascript:Enviar()" class="btn btn-default"><?php echo $msgstr["entrar"]?></a>
+               
+              </div>
+
+             
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><img src=central/images/logoabcd.png> <?php echo $institution_name; ?></h1>
+                  <?php include ("$app_path/common/footer.php");?>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+
+      </div>
+    </div>
+  </body>
+</html>
+
+
+<!--
+
+
+
 <body>
-	<div class="heading">
-		<div class="institutionalInfo">
-			<h1><img src=central/images/logoabcd.png >
-			<?php echo $institution_name; ?></h1>
-		</div>
-		<div class="userInfo"></div>
-		<div class="spacer">&#160;</div>
-	</div>
-	<div class="sectionInfo">
-		<div class="breadcrumb"></div>
-		<div class="actions"></div>
-		<div class="spacer">&#160;</div>
-	</div>
-<form name=administra onsubmit="javascript:return false" method=post action=<?php echo $app_path?>/common/inicio.php?reinicio=s&base=<?php echo $basedefault; ?>>
+
+
+
+
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">	
+
+<form name="administra" onsubmit="javascript:return false" method="post" action="<?php echo $app_path?>/common/inicio.php?reinicio=s&base=<?php echo $basedefault; ?>">
 <input type=hidden name=Opcion value=admin>
 <input type=hidden name=cipar value=acces.par>
-<input type=hidden name=lang value=<?php echo $arrHttp["lang"]?>>
-	<div class="middle login">
-		<div class="loginForm">
-			<div class="boxTop">
-				<div class="btLeft">&#160;</div>
-				<div class="btRight">&#160;</div>
-			</div>
-		<div class="boxContent">
+<value=<?php echo $arrHttp["lang"]?>>
+
+
 <?php
 if (isset($arrHttp["login"]) and $arrHttp["login"]=="N"){
 		echo "
@@ -115,24 +202,58 @@ if (isset($arrHttp["login"]) and $arrHttp["login"]=="N"){
 		";
 }
 ?>
-		<div class="formRow">
+		
 			<label for="user"><?php echo $msgstr["userid"]?></label>
+
 <?php
+
 if (isset($arrHttp["login"]) and $arrHttp["login"]=="N"){
 		echo "
-			<input type=\"text\" name=\"login\" id=\"user\" value=\"\" class=\"textEntry superTextEntry inputAlert\" onfocus=\"this.className = 'textEntry superTextEntry inputAlert textEntryFocus';\" onblur=\"this.className = 'textEntry superTextEntry inputAlert';\" />\n";
+			<input type=\"text\" name=\"login\" id=\"user\" value=\"\" class=\"form-control\" onfocus=\"this.className = 'textEntry superTextEntry inputAlert textEntryFocus';\" onblur=\"this.className = 'textEntry superTextEntry inputAlert';\" />\n";
 }else{
 		echo "
-			<input type=\"text\" name=\"login\" id=\"user\" value=\"\" class=\"textEntry superTextEntry\" onfocus=\"this.className = 'textEntry superTextEntry textEntryFocus';\" onblur=\"this.className = 'textEntry superTextEntry';\" />\n";
+			<input type=\"text\" name=\"login\" id=\"user\" value=\"\" class=\"form-control  \" onfocus=\"this.className = 'textEntry superTextEntry textEntryFocus';\" onblur=\"this.className = 'textEntry superTextEntry';\" />\n";
 }
 ?>
-		</div>
-		<div class="formRow">
+		
 			<label for="pwd"><?php echo $msgstr["password"]?></label>
-			<input type="password" name="password" id="pwd" value="" class="textEntry superTextEntry" onfocus="this.className = 'textEntry superTextEntry textEntryFocus';" onblur="this.className = 'textEntry superTextEntry';" />
-		</div>
-		<div id="formRow3" class="formRow formRowFocus">
-			<label ><?php echo $msgstr["lang"]?></label> <select name=lang class="textEntry singleTextEntry">
+			<input  type="password" name="password" id="pwd" value="" class="textEntry superTextEntry" onfocus="this.className = 'textEntry superTextEntry textEntryFocus';" onblur="this.className = 'textEntry superTextEntry';" />
+		
+
+
+	
+<?php
+if (file_exists("dbpath.dat")){
+	$fp=file("dbpath.dat");
+	echo $msgstr["database_dir"].": <select name=db_path>\n";
+	foreach ($fp as $value){
+		if (trim($value)!=""){
+			$v=explode('|',$value);
+			$v[0]=trim($v[0]);
+			echo "<Option value=".trim($v[0]).">".$v[1]."\n";
+		}
+
+	}
+	echo "</select>	";
+}
+?>
+
+
+			<input type="checkbox" name="newindow" value=
+<?php
+if (isset($open_new_window) and $open_new_window=="Y")
+	echo "Y checked";
+else
+	echo "N";
+?> />
+			<label for="setCookie" class="inline"><?php echo $msgstr["openwindow"]?></label>
+			
+			<a href="javascript:Enviar()" class="btn btn-default"><?php echo $msgstr["entrar"]?></a>
+	
+
+
+<!--		
+			<label><?php echo $msgstr["lang"]?></label> <select name=lang class="textEntry singleTextEntry">
 <?php
 
  	$a=$db_path."lang.tab";
@@ -156,49 +277,17 @@ if (isset($arrHttp["login"]) and $arrHttp["login"]=="N"){
 	}
 ?>
 			</select>
-	/*	</div>
-		<div class="formRow"><br>
-<?php
-if (file_exists("dbpath.dat")){
-	$fp=file("dbpath.dat");
-	echo $msgstr["database_dir"].": <select name=db_path>\n";
-	foreach ($fp as $value){
-		if (trim($value)!=""){
-			$v=explode('|',$value);
-			$v[0]=trim($v[0]);
-			echo "<Option value=".trim($v[0]).">".$v[1]."\n";
-		}
+-->
 
-	}
-	echo "</select><p>";
-}
-?>
-			<input type="checkbox" name="newindow" value=
-<?php
-if (isset($open_new_window) and $open_new_window=="Y")
-	echo "Y checked";
-else
-	echo "N";
-?> />
-			<label for="setCookie" class="inline"><?php echo $msgstr["openwindow"]?></label>
-		</div>
-		<div class="submitRow">
-			<div class="frLeftColumn"></div>
-			<div class="frRightColumn">
-				<a href="javascript:Enviar()" class="defaultButton goButton">
-				<img src="<?php echo $app_path?>/images/icon/defaultButton_next.png" alt="" title="" />
-					<span><strong><?php echo $msgstr["entrar"]?></strong></span>
-				</a>
-			</div>
-		
-		</div>
-	
-	</div>
 
-</div>
-</div>
 </form>
-<?php include ("$app_path/common/footer.php");?>
+          </section>
+        </div>
+       </div> 
+
+   <div class="clearfix"></div>       
+ <div class="footer">
+</div>
 	</body>
 </html>
 
