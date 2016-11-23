7d3cab00703c9365a67250ba83dd2ef3f062721e
lindicell <lindice@hotmail.com>
2016-11-23 15:51:29 -0200

    Corrigido charset ISO8859-1

--- htdocs/central/common/header.php
+++ htdocs/central/common/header.php
@@ -3,8 +3,7 @@
 
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>SuÃ­te Saber<?php if (isset($subtitle))  echo $subtitle?></title>
+		<title>Suíte Saber<?php if (isset($subtitle))  echo $subtitle?></title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">		
 		<meta http-equiv="Expires" content="-1" />
